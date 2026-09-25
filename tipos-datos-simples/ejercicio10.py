070102b1578fabed0c60b50899323169689d36df
ccm777gr <ccm777gr@gmail.com>
2026-09-25 12:05:50 +0200

    hacer ejercicio10

---
+++ tipos-datos-simples/ejercicio10.py
@@ -0,0 +1,7 @@
+payaso = 112
+muneca = 75
+
+num_payasos = int(input("Cuantos payasos hay en el paquete?: "))
+num_munecas = int(input("Cuantas muñecas hay en el paquete?: "))
+
+print(f"El peso total del paquete es {(payaso*num_payasos)+(muneca*num_munecas)} g")
