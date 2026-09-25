da92a9ed659e5eba7b6894aa25963ce644e6b040
ccm777gr <ccm777gr@gmail.com>
2026-09-25 12:07:26 +0200

    Arreglo error guiones ejercicio9

--- tipos-datos-simples/ejercicio9.py
+++ tipos-datos-simples/ejercicio9.py
@@ -1,7 +1,7 @@
 cantidad = float(input("Cuánto quieres invertir?: "))
-interes-año = float(input("Cuál es el interés anual (en %)?: "))
-num-años = float(input("A cuántos años es la inversión?: "))
+interes_año = float(input("Cuál es el interés anual (en %)?: "))
+num_años = float(input("A cuántos años es la inversión?: "))
 
-capital-final = cantidad * (1 + interes-año / 100) ** num-años
+capital_final = cantidad * (1 + interes_año / 100) ** num_años
 
-print(f"El capital obtenido tras {num-años} años es de: {round(capital-final,2)}")
+print(f"El capital obtenido tras {num_años} años es de: {round(capital_final,2)}")
